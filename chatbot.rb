a6bb1dd7b8d985d71571b8ae08b337e5cd33aed9
Chris Ward <chriswarddev@gmail.com>
2014-12-31 19:16:29 +0000

    Colour prompts added

--- chatbot.rb
+++ chatbot.rb
@@ -5,12 +5,23 @@ def get_response(input)
   response.nil? ? 'sorry?' : response % { c1: $1, c2: $2}
 end
 
+class String
+def black;          "\033[30m#{self}\033[0m" end
+def red;            "\033[31m#{self}\033[0m" end
+def green;          "\033[32m#{self}\033[0m" end
+def brown;          "\033[33m#{self}\033[0m" end
+def blue;           "\033[34m#{self}\033[0m" end
+def magenta;        "\033[35m#{self}\033[0m" end
+def cyan;           "\033[36m#{self}\033[0m" end
+def gray;           "\033[37m#{self}\033[0m" end
+end
+
 def user_prompt
-print "User <: "
+print "User <: ".blue
 end
 
 def bot_prompt
- print "Bot ]: "
+ print "Bot ]: ".magenta
 end
 
 RESPONSES = { 'goodbye' => 'bye', 
